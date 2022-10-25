ed620cf3688f2878d4eb4fe8477ac584dfed50a2
DESKTOP-L466AM3\vudo <vudoanh0712@gmail.com>
2022-10-25 13:32:13 +0700

    commit all

--- canthan/app/src/main/java/com/example/shopappn/MainActivity.java
+++ canthan/app/src/main/java/com/example/shopappn/MainActivity.java
@@ -1,16 +1,79 @@
 package com.example.shopappn;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
+
+import android.view.View;
+import android.widget.ActionMenuView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionViewFlipper() {
+        List<String> mangquangcao = new ArrayList<>();
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        for(int i =0;i<mangquangcao.size();i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+
+    }
+
+    private void AnhXa() {
+        toolbar = findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
+        listViewManHinhChinh = findViewById(R.id.listiviewmanhinhchinh);
+        navigationView = findViewById(R.id.navigationview);
+        drawerLayout = findViewById(R.id.drawerlayout);
+
+    }
+    private void ActionBar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
     }
 }
