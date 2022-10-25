d7e2ac430a404715d2e2ae4eee8d4455b83a6d6a
DESKTOP-L466AM3\vudo <vudoanh0712@gmail.com>
2022-10-25 18:47:04 +0700

    commit all

---
+++ canthan/app/src/main/java/com/example/shopappn/adapter/SanPhamMoiAdapter.java
@@ -0,0 +1,59 @@
+package com.example.shopappn.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.shopappn.R;
+import com.example.shopappn.model.SanPhamMoi;
+
+import java.util.List;
+
+public class SanPhamMoiAdapter extends RecyclerView.Adapter<SanPhamMoiAdapter.MyViewHolder> {
+    Context context;
+    List<SanPhamMoi> array;
+
+    public SanPhamMoiAdapter(Context context, List<SanPhamMoi> array) {
+        this.context = context;
+        this.array = array;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sp_moi,parent,false);
+        return new MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        SanPhamMoi sanPhamMoi = array.get(position);
+        holder.txtten.setText(sanPhamMoi.getTensp());
+        holder.txtgia.setText(sanPhamMoi.getGiaSp());
+        Glide.with(context).load(sanPhamMoi.getHinhanh());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return array.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView txtgia,txtten;
+        ImageView   imghinhanh;
+         public MyViewHolder(@NonNull View itemView){
+             super(itemView);
+              txtgia = itemView.findViewById(R.id.itemsp_gia);
+              txtten = itemView.findViewById(R.id.itemsp_ten);
+              imghinhanh = itemView.findViewById(R.id.itemsp_image);
+         }
+    }
+}
---
+++ canthan/app/src/main/java/com/example/shopappn/model/SanPhamMoi.java
@@ -0,0 +1,58 @@
+package com.example.shopappn.model;
+
+public class SanPhamMoi {
+    int id;
+    String tensp;
+    String hinhanh;
+    String giaSp;
+    String mota;
+    int loai;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTensp() {
+        return tensp;
+    }
+
+    public void setTensp(String tensp) {
+        this.tensp = tensp;
+    }
+
+    public String getHinhanh() {
+        return hinhanh;
+    }
+
+    public void setHinhanh(String hinhanh) {
+        this.hinhanh = hinhanh;
+    }
+
+    public String getGiaSp() {
+        return giaSp;
+    }
+
+    public void setGiaSp(String giaSp) {
+        this.giaSp = giaSp;
+    }
+
+    public String getMota() {
+        return mota;
+    }
+
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public int getLoai() {
+        return loai;
+    }
+
+    public void setLoai(int loai) {
+        this.loai = loai;
+    }
+}
