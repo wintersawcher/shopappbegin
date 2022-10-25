dd457edb74fcc30265bd1668bfc1725a501a6665
DESKTOP-L466AM3\vudo <vudoanh0712@gmail.com>
2022-10-25 13:59:08 +0700

    commit ok

--- canthan/app/src/main/java/com/example/shopappn/activity/MainActivity.java
+++ canthan/app/src/main/java/com/example/shopappn/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.shopappn;
+package com.example.shopappn.activity;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ActionMenuView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -18,6 +20,7 @@ import android.widget.ListView;
 import android.widget.ViewFlipper;
 
 import com.bumptech.glide.Glide;
+import com.example.shopappn.R;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
@@ -42,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private void ActionViewFlipper() {
         List<String> mangquangcao = new ArrayList<>();
         mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
+        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
         for(int i =0;i<mangquangcao.size();i++){
             ImageView imageView = new ImageView(getApplicationContext());
             Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
@@ -52,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
         }
         viewFlipper.setFlipInterval(3000);
         viewFlipper.setAutoStart(true);
+        Animation slide_in  = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation slide_out  = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_left);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setInAnimation(slide_out);
 
     }
 
