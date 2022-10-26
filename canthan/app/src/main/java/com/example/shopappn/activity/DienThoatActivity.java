5841c31fd7ca66fa314b19f2c4314ed08cc25389
DESKTOP-L466AM3\vudo <vudoanh0712@gmail.com>
2022-10-26 09:46:18 +0700

    commit all

---
+++ canthan/app/src/main/java/com/example/shopappn/activity/DienThoatActivity.java
@@ -0,0 +1,14 @@
+package com.example.shopappn.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DienThoatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dien_thoat);
+    }
+}
